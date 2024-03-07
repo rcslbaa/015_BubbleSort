c0973974199107c432ff7b6857be9d8c3b1fcad5
rcslbaa <ifasmarani3125@gmail.com>
2024-03-07 20:10:15 +0700

    menambahkan fungsi display

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -51,6 +51,23 @@ void bubbleSortArray() { //procedur untuk mengurutkan array dengan metode bublle
 
     } while (pass <= n - 1); //step 5
 }
+void display() {
+    cout << endl;
+    cout << "===================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "===================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j]; // Output setiap element array pada garis baru
+        if (j < n - 1) {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Jumlah pass = " << n - 1 << endl; //menampilkan total angka dari elemen yang benar
+    cout << endl;
+}
 
 
 
