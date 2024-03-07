b724e3921ea7068381e4494c8f6ec22d3f779b96
rcslbaa <ifasmarani3125@gmail.com>
2024-03-07 20:06:26 +0700

    menambahkan variable global

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -7,25 +7,3 @@ using namespace std;
 int arr[20];
 int n;
 
-void input() {
-    while (true) {
-        cout << "Masukan banyaknya elemen pada array : ";
-        cin >> n;
-        if (n <= 20)
-            break;
-        else {
-            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
-        }
-    }
-    cout << endl;                           //output baris kosong
-    cout << "===============" << endl;      //output ke layar
-    cout << "Masukan Elemen Array" << endl; //output ke layar
-    cout << "===============" << endl;      //output ke layar
-
-    for (int i = 0; i < n; i++) { //looping dengan i dimulai dari 0 hingga n-1
-        cout << "Data ke-" << (i + 1) << ": "; //output ke layar
-        cin >> arr[i];                         //input ke pengguna
-
-    }
-}
-
