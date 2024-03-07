7389df5ee64373f950e547ed0b123c96c982895a
rcslbaa <ifasmarani3125@gmail.com>
2024-03-07 20:08:33 +0700

    menambahkan fungsi bubblesortarray

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -28,5 +28,29 @@ void input() {
 
     }
 }
+void bubbleSortArray() { //procedur untuk mengurutkan array dengan metode bublle sort
+    int pass = 1; // step 1
+
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) { // step 2
+            if (arr[j] > arr[j + 1]) { //step 3
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass = pass + 1; //step 4
+
+        cout << "\nPass " << pass - 1 << ": ";  //output ke layar
+        for (int k = 0; k < n; k++) {           //looping nilai k dimulai dari 0 hingga n-1
+            cout << arr[k] << " ";              //output ke layar
+
+
+        }
+        cout << endl;
+
+    } while (pass <= n - 1); //step 5
+}
+
 
 
